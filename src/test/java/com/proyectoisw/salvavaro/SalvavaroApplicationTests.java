0cc2419577490145607f5daae35c277e33c62f37
raulito <raul.ramos.1992.1992@gmail.com>
2023-01-05 12:54:23 -0600

    Funciona contenido estático

---
+++ src/test/java/com/proyectoisw/salvavaro/SalvavaroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoisw.salvavaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalvavaroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
